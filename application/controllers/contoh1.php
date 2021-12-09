12f1ec84d89872531056ae7c1940dd3462a767d7
sellywil <wilfiantiselly62@gmail.com>
2021-12-09 16:30:44 +0700

    pertemuan 2

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,8 +1,12 @@
-<?php 
+<?php
+class contoh1 extends CI_Controller
 {
-	echo "<h1>Perkenalkan</1>";
+	public function index()
+	{
+	echo "<h1>Perkenalkan</h1>";
 	echo "Nama saya Selly Wilfianti 
 	      Saya tinggal di daerah Bogor 
 	      Olahraga yang saya sukai adalah 
 	      Mendaki";
+	  }
 }
