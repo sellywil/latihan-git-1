794040deb43435ac501dc63b32c6ee15a83bb6cc
sellywil <wilfiantiselly62@gmail.com>
2021-12-13 15:52:03 +0700

    pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : selly</li>
+ <li>Nama Belakang : wilfianti</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Pajajaran no 40 Bogor</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bogor</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
